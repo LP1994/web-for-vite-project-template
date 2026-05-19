1ad307d8ab126384c9412bf1239933bebf418253
林鹏 <1227839175@qq.com>
2026-05-19 12:14:53 +0800

    log: 日常更新。

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/**
+ * Project: web-for-vite-project-template
+ * FileDirPath: vite.config.js
+ * Author: 12278
+ * Email: 1227839175@qq.com
+ * IDE: WebStorm
+ * CreateDate: 2026-05-16 00:00:00 星期六
+ */
+
+'use strict';
+
+export default {};
